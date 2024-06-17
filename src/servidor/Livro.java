6cdea96bff4444236d03c7188839907a69a8aafd
João Pedro de Oliveira Teles <80984493+joaopedroteles@users.noreply.github.com>
2024-06-17 19:10:59 -0300

    Update Livro.java

--- src/servidor/Livro.java
+++ src/servidor/Livro.java
@@ -2,11 +2,20 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Livro {
-    private String autor;
     private String titulo;
+    private String autor;
     private String genero;
     private int exemplares;
+    private int contadorAluguel;
 
+
+    public int getContadorAluguel() {
+        return contadorAluguel;
+    }
+
+    public void setContadorAluguel(int contadorAluguel) {
+        this.contadorAluguel = contadorAluguel;
+    }
     // Construtor padrão
     public Livro() {}
 
