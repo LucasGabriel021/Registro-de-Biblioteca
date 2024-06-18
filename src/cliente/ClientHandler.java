76ef71703352e7cd7bcd63138898c9059b1fa8a6
leocasagrande <leonardocasagrandesilva@hotmail.com>
2024-06-18 15:12:52 -0300

    mudancas nas classes servidor e clientehandler

---
+++ src/cliente/ClientHandler.java
@@ -0,0 +1,131 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class ClientHandler extends Thread{
+
+    Servidor servidor = new Servidor();
+    private final Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+
+            String request;
+            while ((request = in.readLine()) != null) {
+                String[] parts = request.split(";");
+                System.out.println(Arrays.toString(parts));
+                if (parts.length < 1) {
+                    out.println("Comando inválido!");
+                    continue;
+                }
+                String command = parts[0];
+
+                switch (command) {
+                    case "LISTA":
+                        out.println(servidor.livros);
+                        break;
+
+                    case "ALUGUEL":
+                        if (parts.length < 2) {
+                            out.println("Comando inválido!");
+                            break;
+                        }
+                        String tituloAlugar = parts[1];
+                        rentBook(tituloAlugar, out);
+                        break;
+
+                    case "DEVOLUCAO":
+                        if (parts.length < 2) {
+                            out.println("Comando inválido!");
+                            break;
+                        }
+                        String tituloDevolver = parts[1];
+                        returnBook(tituloDevolver, out);
+                        break;
+
+                    case "ADICIONA":
+                        if (parts.length < 5) {
+                            out.println("Comando inválido!");
+                            break;
+                        }
+                        String tituloAdd = parts[1];
+                        String autor = parts[2];
+                        String generoAdd = parts[3];
+                        int exemplares = Integer.parseInt(parts[4]);
+                        addBook(new Livro(tituloAdd, autor, generoAdd, exemplares), out);
+                        break;
+
+                    case "GENERO":
+                        if (parts.length < 2) {
+                            out.println("Comando inválido!");
+                            break;
+                        }
+
+                        String generoSearch = parts[1];
+                        listBooksByGenre(generoSearch, out);
+                        break;
+
+                    default:
+                        out.println("Comando não reconhecido.");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listBooksByGenre(String genero, PrintWriter out) {
+        boolean found = false;
+        for (Livro livro : servidor.livros) {
+            if (livro.getGenero().equalsIgnoreCase(genero)) {
+                out.println(livro);
+                found = true;
+            }
+        }
+        if (!found) {
+            out.println("Nenhum livro encontrado para o gênero: " + genero);
+        }
+    }
+
+    private void rentBook(String titulo, PrintWriter out) {
+        for (Livro livro : servidor.livros) {
+            if (livro.getTitulo().equalsIgnoreCase(titulo) && livro.getExemplares() > 0) {
+                livro.setExemplares(livro.getExemplares() - 1);
+                servidor.saveLivros();
+                out.println("Alugado: " + livro);
+                return;
+            }
+        }
+        out.println("Livro não disponível .");
+    }
+
+    private void returnBook(String titulo, PrintWriter out) {
+        for (Livro livro : servidor.livros) {
+            if (livro.getTitulo().equalsIgnoreCase(titulo)) {
+                livro.setExemplares(livro.getExemplares() + 1);
+                servidor.saveLivros();
+                out.println("Devolvido: " + livro);
+                return;
+            }
+        }
+        out.println("Livro não encontrado.");
+    }
+
+    private void addBook(Livro livro, PrintWriter out) {
+        System.out.println("Adicionando livro: " + livro);
+        servidor.livros.add(livro);
+        servidor.saveLivros();
+        out.println("Adicionado: " + livro);
+        System.out.println("Lista de livros após adição: " + servidor.livros);
+    }
+
+}
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Servidor {
     private static final int PORT = 12345;
-    private static List<Livro> livros = new ArrayList<>();
+    public static List<Livro> livros = new ArrayList<>();
     // private static final String FILE_PATH = "livros.json";
     private static final ObjectMapper mapper = new ObjectMapper();
 
@@ -24,7 +24,7 @@ public class Servidor {
         }
     }
 
-    private static void loadLivros() {
+    public static void loadLivros() {
         try (InputStream inputStream = new FileInputStream("src/servidor/livros.json")) {
             livros = mapper.readValue(inputStream, new TypeReference<List<Livro>>() {});
             System.out.println(livros);
@@ -33,135 +33,13 @@ public class Servidor {
         }
     }
 
-    private static void saveLivros() {
+    public static void saveLivros() {
         try {
             mapper.writeValue(new File("src/servidor/livros.json"), livros);
             System.out.println("Livros salvos com sucesso no arquivo ");
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Erro ao salvar os livros: " + e.getMessage());
-        }
-    }
-
-    private static class ClientHandler extends Thread {
-        private final Socket socket;
-
-        public ClientHandler(Socket socket) {
-            this.socket = socket;
-        }
-
-        @Override
-        public void run() {
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-
-                String request;
-                while ((request = in.readLine()) != null) {
-                    String[] parts = request.split(";");
-                    System.out.println(Arrays.toString(parts));
-                    if (parts.length < 1) {
-                        out.println("Comando inválido!");
-                        continue;
-                    }
-                    String command = parts[0];
-
-                    switch (command) {
-                        case "LISTA":
-                            out.println(livros);
-                            break;
-
-                        case "ALUGUEL":
-                            if (parts.length < 2) {
-                                out.println("Comando inválido!");
-                                break;
-                            }
-                            String tituloAlugar = parts[1];
-                            rentBook(tituloAlugar, out);
-                            break;
-
-                        case "DEVOLUCAO":
-                            if (parts.length < 2) {
-                                out.println("Comando inválido!");
-                                break;
-                            }
-                            String tituloDevolver = parts[1];
-                            returnBook(tituloDevolver, out);
-                            break;
-
-                        case "ADICIONA":
-                            if (parts.length < 5) {
-                                out.println("Comando inválido!");
-                                break;
-                            }
-                            String tituloAdd = parts[1];
-                            String autor = parts[2];
-                            String generoAdd = parts[3];
-                            int exemplares = Integer.parseInt(parts[4]);
-                            addBook(new Livro(tituloAdd, autor, generoAdd, exemplares), out);
-                            break;
-
-                        case "GENERO":
-                            if (parts.length < 2) {
-                                out.println("Comando inválido!");
-                                break;
-                            }
-
-                            String generoSearch = parts[1];
-                            listBooksByGenre(generoSearch, out);
-                            break;
-
-                        default:
-                            out.println("Comando não reconhecido.");
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        private void listBooksByGenre(String genero, PrintWriter out) {
-            boolean found = false;
-            for (Livro livro : livros) {
-                if (livro.getGenero().equalsIgnoreCase(genero)) {
-                    out.println(livro);
-                    found = true;
-                }
-            }
-            if (!found) {
-                out.println("Nenhum livro encontrado para o gênero: " + genero);
-            }
-        }
-        
-        private void rentBook(String titulo, PrintWriter out) {
-            for (Livro livro : livros) {
-                if (livro.getTitulo().equalsIgnoreCase(titulo) && livro.getExemplares() > 0) {
-                    livro.setExemplares(livro.getExemplares() - 1);
-                    saveLivros();
-                    out.println("Alugado: " + livro);
-                    return;
-                }
-            }
-            out.println("Livro não disponível .");
-        }
-
-        private void returnBook(String titulo, PrintWriter out) {
-            for (Livro livro : livros) {
-                if (livro.getTitulo().equalsIgnoreCase(titulo)) {
-                    livro.setExemplares(livro.getExemplares() + 1);
-                    saveLivros();
-                    out.println("Devolvido: " + livro);
-                    return;
-                }
-            }
-            out.println("Livro não encontrado.");
-        }
-
-        private void addBook(Livro livro, PrintWriter out) {
-            System.out.println("Adicionando livro: " + livro);
-            livros.add(livro);
-            saveLivros();
-            out.println("Adicionado: " + livro);
-            System.out.println("Lista de livros após adição: " + livros);
         }
     }
 }
